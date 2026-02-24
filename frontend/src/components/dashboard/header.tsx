8b8a65090d45e49a11270cc87cab2b4822b9f4ad
believehertz <believehertz@gmail.com>
2026-02-24 18:53:22 +0300

    Update header.tsx

--- frontend/src/components/dashboard/header.tsx
+++ frontend/src/components/dashboard/header.tsx
@@ -10,7 +10,7 @@ interface HeaderProps {
   onLogout: () => void;
 }
 
-export const header: React.FC<HeaderProps> = ({ 
+export const Header: React.FC<HeaderProps> = ({ 
   darkMode, 
   username, 
   notificationCount = 0,
